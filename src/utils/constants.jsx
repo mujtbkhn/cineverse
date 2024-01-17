1ca3a41d89d890285be9cb97ea1b588613eb752d
abdul rehman mujtaba <mujjukhn538@gmail.com>
2024-01-17 15:00:33 +0530

    pushed imp keys to .env file

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,20 +1,27 @@
+export const LOGO =
+  "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 
-export const LOGO = 'https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png'
+export const BACKGROUND =
+  "https://assets.nflxext.com/ffe/siteui/vlv3/c38a2d52-138e-48a3-ab68-36787ece46b3/eeb03fc9-99c6-438e-824d-32917ce55783/IN-en-20240101-popsignuptwoweeks-perspective_alpha_website_medium.jpg";
 
-export const BACKGROUND = 'https://assets.nflxext.com/ffe/siteui/vlv3/c38a2d52-138e-48a3-ab68-36787ece46b3/eeb03fc9-99c6-438e-824d-32917ce55783/IN-en-20240101-popsignuptwoweeks-perspective_alpha_website_medium.jpg'
-
-export const USER_AVATAR = "https://lh3.googleusercontent.com/ogw/ANLem4YgkwRxj6cLaaWmUEkKmnJItfo6NkItO569WIPLQw=s32-c-mo"
+export const USER_AVATAR =
+  "https://lh3.googleusercontent.com/ogw/ANLem4YgkwRxj6cLaaWmUEkKmnJItfo6NkItO569WIPLQw=s32-c-mo";
 
 export const OPTIONS = {
-    method: 'GET',
-    headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZjNlMmRiMjA5Y2UwMjY5MDRmZjMzYWJkMWM1NTU3OCIsInN1YiI6IjY1OWZhMTFmOTA3ZjI2MDEyMjIwZTA2MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nZ8TO-EaAryQd4nbVT2-SfINk8RC4lUTsX8vP1q4j1Q'
-    }
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer " + process.env.REACT_APP_TMDB_KEY,
+  },
 };
 
-export const IMG_CDN = 'https://image.tmdb.org/t/p/w300';
+export const IMG_CDN = "https://image.tmdb.org/t/p/w300";
 
-export const SUPPORTED_LANGUAGES = [{identifier : "en", name : "English"}, {identifier : "hindi", name : "Hindi"}, {identifier : "arabic", name : "Arabic"}]
+export const SUPPORTED_LANGUAGES = [
+  { identifier: "en", name: "English" },
+  { identifier: "hindi", name: "Hindi" },
+  { identifier: "arabic", name: "Arabic" },
+];
 
-export const OPENAI_KEY = "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";
+export const OPENAI_KEY = process.env.REACT_APP_OPENAI_KEY;
