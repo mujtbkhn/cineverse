840981ec1b3790df5a75f79d7406f581314b3538
abdul rehman mujtaba <mujjukhn538@gmail.com>
2024-10-15 11:29:00 +0530

    removed dangerouslyAllowBrowser Tag

--- src/utils/openai.jsx
+++ src/utils/openai.jsx
@@ -2,7 +2,7 @@ import OpenAI from 'openai';
 import { OPENAI_KEY } from './constants';
 
 const openai = new OpenAI({
-  apiKey: OPENAI_KEY, dangerouslyAllowBrowser: true 
+  apiKey: OPENAI_KEY,
 });
 
 export default openai
